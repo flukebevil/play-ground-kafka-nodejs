25f1017dd9ccce22a478c8a0e28695e914874008
sarawoot pokkam <flukebevil@gmail.com>
2020-03-12 16:17:15 +0700

    init

---
+++ consumer.js
@@ -0,0 +1,11 @@
+const kafka = require("kafka-node");
+const client = new kafka.KafkaClient("0.0.0.0:2181" );
+const topic = [
+  {
+    topic: "test-node"
+  }
+];
+const options = { autoCommit: false };
+const consumer = new kafka.Consumer(client, topic, options);
+
+consumer.on("message", message => console.log(message));
